3cdc73935133d42f4683f67d79e19790264984ee
José Victor Zeller <josevictorzg@gmail.com>
2022-04-05 20:55:23 -0300

    refactor: reuse code in commit history view

--- lib/views/commit_history.ex
+++ lib/views/commit_history.ex
@@ -5,56 +5,87 @@ defmodule TinyCareTerminal.App.Views.CommitHistory do
 
   import Ratatouille.View
 
-  def render(_model, window) do
-    today =
-      row do
-        column(size: 6) do
-          panel(title: " 📆  Today ", height: round(window.height * 0.3)) do
-            label(content: "Project path: ~/Projects/Stone", color: :green)
-            label(content: "")
+  alias Ratatouille.Renderer.Element
+  alias TinyCareTerminal.Models.Commit
+  alias TinyCareTerminal.Models.CommitHistory
 
-            row do
-              column(size: 6) do
-                for n <- 1..5 do
-                  label do
-                    text(content: "055#{n + 1}71#{n}", color: :red)
-                    text(content: " - ")
-                    text(content: "commit description #{n}")
-                    text(content: " ")
-                    text(content: "(#{n} hour(s) ago)", color: :green)
-                  end
-                end
-              end
-            end
-          end
-        end
-      end
+  @spec render(CommitHistory.t(), map()) :: {Element.t(), Element.t()}
+  def render(%CommitHistory{} = history, window) do
+    %{today: today_commits, last_week: last_week_commits, repo_path: path} = history
 
-    # Week commits
-    week =
-      row do
-        column(size: 6) do
-          panel(title: " 🗓  This Week ", height: :fill) do
-            label(content: "Project path: ~/Projects/Stone", color: :green)
-            label()
+    today_opts = [
+      column_size: 6,
+      height: round(window.height * 0.3),
+      title: " 📆  Today ",
+      path: path
+    ]
 
-            row do
-              column(size: 6) do
-                for n <- 1..9 do
-                  label do
-                    text(content: "055#{n + 1}71#{n}", color: :red)
-                    text(content: " - ")
-                    text(content: "commit description #{n}")
-                    text(content: " ")
-                    text(content: "(#{n} hour(s) ago)", color: :green)
-                  end
-                end
-              end
-            end
-          end
-        end
-      end
+    last_week_opts = [
+      column_size: 6,
+      height: :fill,
+      title: " 🗓  Last Week ",
+      path: path
+    ]
+
+    today = render_commits(today_commits, today_opts)
+
+    week = render_commits(last_week_commits, last_week_opts)
 
     {today, week}
+  end
+
+  defp render_commits([], opts) do
+    height = opts[:height]
+    title = opts[:title]
+    column_size = opts[:column_size]
+    path = opts[:path]
+
+    content = """
+    No commits found for path '#{path}'. Are you sure it's a Git repo?
+
+    Tip: Make sure to provide the full path to the repo and if ou are using shorthands like
+    '~/SomeFolder/SomeRepo', don't use strings when exporting the environment variable.
+    """
+
+    row do
+      column(size: column_size) do
+        panel(title: title, height: height) do
+          label(
+            content: content,
+            color: :green
+          )
+        end
+      end
+    end
+  end
+
+  defp render_commits(commits, opts) do
+    height = opts[:height]
+    title = opts[:title]
+    column_size = opts[:column_size]
+    path = opts[:path]
+
+    row do
+      column(size: column_size) do
+        panel(title: title, height: height) do
+          label(content: "Project path: #{path}", color: :green)
+          label(content: "")
+
+          row do
+            column(size: column_size) do
+              for %Commit{} = commit <- commits do
+                label do
+                  text(content: commit.hash, color: :red)
+                  text(content: " - ")
+                  text(content: commit.message)
+                  text(content: " ")
+                  text(content: commit.relative_date, color: :green)
+                end
+              end
+            end
+          end
+        end
+      end
+    end
   end
 end
