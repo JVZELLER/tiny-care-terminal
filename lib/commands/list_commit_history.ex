16491c358f7b113ec6e5e807f0a195907d90d097
José Victor Zeller <josevictorzg@gmail.com>
2022-04-05 20:54:55 -0300

    refactor: simplify comits history listing

--- lib/commands/list_commit_history.ex
+++ lib/commands/list_commit_history.ex
@@ -3,14 +3,14 @@ defmodule TinyCareTerminal.Commands.ListCommitHistory do
   Fetches the Git history from a project.
   """
 
-  alias TinyCareTerminal.App.Models.Commit
-  alias TinyCareTerminal.App.Models.CommitHistory
+  alias TinyCareTerminal.Models.Commit
+  alias TinyCareTerminal.Models.CommitHistory
 
   @format "%h`%s`(%cd)"
 
-  @spec execute() :: [Commit.t()]
+  @spec execute() :: CommitHistory.t()
   def execute do
-    (git_repo?() and list_commits()) || []
+    (git_repo?() and list_commits()) || %CommitHistory{repo_path: path()}
   end
 
   defp git_repo? do
@@ -30,18 +30,16 @@ defmodule TinyCareTerminal.Commands.ListCommitHistory do
     today_commits = list_today_commits(repo_path)
     last_week_commits = list_last_week_commits(repo_path)
 
-    CommitHistory.create(%{today: today_commits, last_week: last_week_commits})
+    CommitHistory.create(%{
+      today: today_commits,
+      last_week: last_week_commits,
+      repo_path: repo_path
+    })
   end
 
-  def list_today_commits(path) do
-    today = Date.to_iso8601(Date.utc_today())
-    list_commits(path, ["--since=#{today}"])
-  end
+  def list_today_commits(path), do: list_commits(path, ["--since=midnight"])
 
-  defp list_last_week_commits(path) do
-    last_week = Date.utc_today() |> Date.add(-7) |> Date.to_iso8601()
-    list_commits(path, ["--since=#{last_week}"])
-  end
+  defp list_last_week_commits(path), do: list_commits(path, ["--since=last_week"])
 
   defp list_commits(path, since_opt) do
     user = get_git_user(path)
